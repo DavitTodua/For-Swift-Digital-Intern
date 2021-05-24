b77f57395a206e4d627fa2d7c740c42fcd204f39
DavitTodua <82881089+DavitTodua@users.noreply.github.com>
2021-05-24 16:33:49 +0400

    Add files via upload
    ვტვირთავ მთელ პროექტს, სადაც source - ში შესრულებულია ყველა დავალება.
    DataStructure - მონაცემთა კლასი სადაც O(1) ში ხდება მონაცემის წაშლა
    Methods - მოთხოვნილი მეთოდებიდან 1-5 ის ჩათვლით
    WebsiteThing - კლასი რომელსაც აქვს მეთოდი დაგვიბრუნოს გაცვლითი კურსი გადაცემული ვალუტების.

---
+++ FORINTERN/src/WebsiteThing.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class WebsiteThing {
+    private double intValue1;
+    private double intValue2;
+    private Object IllegalArgumentException;
+
+    public WebsiteThing() {
+        intValue1 = -1;
+        intValue2 = -2;
+        IllegalArgumentException = new IllegalArgumentException();
+    }
+    public double getCourse(String val1, String val2) throws Throwable {
+
+        URL url = new URL("http://www.nbg.ge/rss.php");
+        Scanner sc = new Scanner(url.openStream());
+
+        while (sc.hasNext()) {
+            String curr = sc.next().toString();
+
+            if(curr.contains(val1)) {
+                String currentVal;
+                while(true) {
+                    currentVal = sc.next();
+                    if(currentVal.contains("</td>")) break;
+                }
+                currentVal = sc.next();
+                currentVal = currentVal.replaceAll("<[^>]*>", "");
+                intValue1 = Double.valueOf(currentVal);
+            }
+            if(curr.contains(val2)) {
+                String currentVal;
+                while(true) {
+                    currentVal = sc.next();
+                    if(currentVal.contains("</td>")) break;
+                }
+                currentVal = sc.next();
+                currentVal = currentVal.replaceAll("<[^>]*>", "");
+                intValue2 = Double.valueOf(currentVal);
+            }
+
+            if( intValue1 >= 0 && intValue2 >= 0) break;
+
+            }
+        if( intValue1 >= 0 && intValue2 >= 0) {
+            double result = intValue1 / intValue2;
+            return result;
+        } else throw (Throwable) IllegalArgumentException;
+    }
+
+}
